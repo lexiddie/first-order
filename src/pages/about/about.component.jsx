236c049017fa01aa757d514fcfe567aa0ed50d6d
Lex <37273428+lexiddie@users.noreply.github.com>
2021-01-03 18:19:38 +0700

    completed

--- src/pages/about/about.component.jsx
+++ src/pages/about/about.component.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
 
-const About = (props) => {
+import './about.styles.scss';
+
+const About = () => {
   return (
     <>
-      <h2>About Page</h2>
+      <div className='about'>
+        <h2>About Page</h2>
+        <p>
+          The First Order, also known simply as the Order, was an autocratic military junta that formed from the remnants of the Galactic Empire during the New Republic Era. Born in the Unknown
+          Regions of the galaxy, the First Order claimed legitimacy as a successor state to the Empire. As with its predecessor, the First Order was dominated by a ruling political and military human
+          elite. As a polity, the First Order was ruled by a Supreme Leader from their mobile headquarters instead of a traditional planetary capital. It grew from a hermit state to the dominant
+          galactic power, having gradually colonized large swathes of the Unknown Regions and boosted by secessionists from the New Republic, annexing the territory that would serve a crucial role in
+          the regime's plan to eventually seize the galaxy at large. Yet despite their conquest of the major star systems, the First Order's military rule was met with resistance and rebellion.
+        </p>
+        <br />
+        <p>As the First and Final Order have been defeated by The Resistance in the Rise of Skywalker. Since then the First Order has opened an online store to sell luxurious watches⏱.</p>
+      </div>
     </>
   );
 };
--- src/pages/contact/contact.component.jsx
+++ src/pages/contact/contact.component.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
 
+import './contact.styles.scss';
+
 const Contact = (props) => {
   return (
     <>
-      <h2>Contact Page</h2>
+      <div className='contact'>
+        <h2>Contact Page</h2>
+        <p>You can use your Force to contact the Supreme Leader Kylo Ren.😲</p>
+        <br />
+        <p>
+          Kylo Ren, the grandson of Darth Vader, became the new Supreme Leader after he assassinated Snoke in 34 ABY. Supported by Allegiant General Enric Pryde, Ren led the First Order on a campaign
+          of galactic conquest and hunted the remnants of the Resistance.
+        </p>
+      </div>
     </>
   );
 };
