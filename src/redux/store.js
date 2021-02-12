933e4990a3383ec1e0e8d045f7315f6b6dac0836
Lex <37273428+lexiddie@users.noreply.github.com>
2021-02-12 17:19:11 +0700

    added env back

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,7 @@
 import { createStore, applyMiddleware } from 'redux';
 import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
+import dotenv from 'dotenv';
 
 import rootReducer from './root-reducer';
 
@@ -8,6 +9,8 @@ const middlewares = [];
 
 if (process.env.NODE_ENV === 'development') {
   middlewares.push(logger);
+} else {
+  dotenv.config();
 }
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
